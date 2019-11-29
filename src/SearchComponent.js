c6c403b65b741209300e4b3e284703b683e33658
Jason Tan <jasontan056@gmail.com>
2019-11-30 00:03:20 +0800

    More styling for the search drawer

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -9,8 +9,15 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   drawerContents: {
     width: "95vw",
+    height: "100%",
+    display: "grid",
+    gridTemplateRows: "min-content auto",
     background:
       "linear-gradient(90deg, rgba(105,252,69,1) 0%, rgba(69,252,73,0.30996148459383754) 100%)"
+  },
+  searchResults: {
+    width: "inherit",
+    overflow: "auto"
   },
   ul: {
     listStyle: "none",
@@ -61,9 +68,13 @@ export default function SearchComponent(props) {
       onClose={props.onDrawerClosed}
     >
       <div className={classes.drawerContents}>
-        <Link to={props.returnUrl}>Back</Link>
-        <SearchBar handleSubmit={handleSubmit} />
-        <ul className={classes.ul}>{songItems}</ul>
+        <div>
+          <Link to={props.returnUrl}>Back</Link>
+          <SearchBar handleSubmit={handleSubmit} />
+        </div>
+        <div className={classes.searchResults}>
+          <ul className={classes.ul}>{songItems}</ul>
+        </div>
       </div>
     </SwipeableDrawer>
   );
