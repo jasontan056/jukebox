adb46092541c9db309c602f06eb60049960c95f0
Jason Tan <jasontan056@gmail.com>
2019-11-30 18:33:50 +0800

    Fix db file path

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "../build")));
 app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 
-const dbFile = path.join(__dirname, "../data", "sqlite.db");
+const dbFile = path.join(__dirname, "../.data", "sqlite.db");
 const dao = new Dao(dbFile);
 
 handleSockets(io, dao);
